a5011d6c1e27ca0c0d2920dea6a88c229c9a5553
suprunyuk-a <chaserenergy@gmail.com>
2022-07-27 17:07:23 +0300

    add_task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const elemDiv = document.querySelector(".gallery");
+const elemMass =
+  '<ul class="gallery">' +
+  galleryItems
+    .map(
+      (index) => `<li class="gallery__item"> 
+                  <a class="gallery__link" href="${index.original}">
+                  <img class="gallery__image" src=${index.preview} alt=${index.description} source="${index.original}"/> 
+                  </a> </li>`
+    )
+    .join("") +
+  "</ul>";
+elemDiv.insertAdjacentHTML("beforeend", elemMass);
+
+let elem;
+const handleClick = (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    elem = basicLightbox.create(
+      `<img src=${event.target.getAttribute("source")} />`,
+      {
+        onShow: (elem) => {
+          const listener = (event) => {
+            if (event.key === "Escape") {
+              elem.close();
+              elemDiv.removeEventListener("keyup", listener);
+            }
+          };
+          elemDiv.addEventListener("keyup", listener);
+        },
+      }
+    );
+    elem.show();
+  } else {
+    event.stopPropagation();
+  }
+};
+elemDiv.addEventListener("click", handleClick);
